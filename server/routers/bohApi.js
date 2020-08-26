a2997d3e780fb439a4eccda74d7505f320838590
malong <malong94@163.com>
2020-08-26 14:14:00 +0800

    删除冗余/修正任务进度

--- server/routers/bohApi.js
+++ server/routers/bohApi.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../model/model').models;
-var ecId = "xydSXCsZ38TqQshY7";
-var domainId = "PFSvkHRcArfzJEuZt";
-var commons = require('../model/common');
-var _ = require('underscore')
-const utils = require('../utils');
 router.post('/notice', function (req, res, next) {
-    console.log('===notice',req.body)
     let { type, bill_num, stable_code, rtable_code } = req.body;
-    console.log('===============','type',type,'bill_num',bill_num, stable_code, rtable_code)
     if (!type) return res.json({ code: 4002, message: "type值不能为空" });
     if (!bill_num) return res.json({ code: 4002, message: "订单号不能为空" });
     let resData = { code: 200, type, bill_num };
